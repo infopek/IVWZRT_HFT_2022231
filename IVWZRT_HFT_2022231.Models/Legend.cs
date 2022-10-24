df18e4975129e65f684fd78d439090f1b24dc974
infopek <10saero01@gmail.com>
2022-10-25 01:24:28 +0200

    Create Legend model

---
+++ IVWZRT_HFT_2022231.Models/Legend.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace IVWZRT_HFT_2022231.Models
+{
+    public class Legend
+    {
+        public Legend() { }
+        public Legend(string line)
+        {
+            string[] split = line.Split('@');
+
+            LegendId = int.Parse(split[0]);
+
+            Name = split[1];
+            Role = split[2];
+
+            Passive = split[3];
+            Tactical = split[4];
+            Ultimate = split[5];
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LegendId { get; set; }
+
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+        public string Passive { get; set; }
+        public string Tactical { get; set; }
+        public string Ultimate { get; set; }
+
+        public virtual ICollection<Player> Players { get; set; }
+    }
+}
