00c9ecb2a66832b3075987fb3b2dd33c985f70ee
infopek <10saero01@gmail.com>
2022-10-24 21:35:57 +0200

    Add project files.

---
+++ IVWZRT_HFT_2022231.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IVWZRT_HFT_2022231.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ IVWZRT_HFT_2022231.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IVWZRT_HFT_2022231.Test
+{
+    public class Class1
+    {
+    }
+}
