92414dd3c51cce3f18f9e0cf2865fa683a9de412
infopek <10saero01@gmail.com>
2022-10-25 21:32:44 +0200

    Create Repository class for EndGameStat

---
+++ IVWZRT_HFT_2022231.Repository/ModelRepositories/StatRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+using IVWZRT_HFT_2022231.Models;
+
+namespace IVWZRT_HFT_2022231.Repository
+{
+    public class StatRepository : Repository<EndGameStat>, IRepository<EndGameStat>
+    {
+        public StatRepository(ApexDbContext context)
+            : base(context) { }
+
+        public override EndGameStat Read(int id)
+        {
+            return _context.Stats.FirstOrDefault(p => p.StatId == id);
+        }
+        public override void Update(EndGameStat item)
+        {
+            var old = Read(item.StatId);
+            foreach (var prop in old.GetType().GetProperties())
+                prop.SetValue(old, prop.GetValue(item));
+
+            _context.SaveChanges();
+        }
+    }
+}
