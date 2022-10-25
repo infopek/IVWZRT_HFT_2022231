664b090dcd60740f3b0b2a88fb9312eab5abcf6e
infopek <10saero01@gmail.com>
2022-10-25 21:30:57 +0200

    Create Repository interface

---
+++ IVWZRT_HFT_2022231.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+using IVWZRT_HFT_2022231.Models;
+
+namespace IVWZRT_HFT_2022231.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> ReadAll();
+        T Read(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
