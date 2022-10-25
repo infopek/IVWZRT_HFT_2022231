c9f259f6da4d77ee753e1b5e23cd92a24804cf6f
infopek <10saero01@gmail.com>
2022-10-25 21:32:34 +0200

    Create Repository class for Player

---
+++ IVWZRT_HFT_2022231.Repository/ModelRepositories/PlayerRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+using IVWZRT_HFT_2022231.Models;
+
+namespace IVWZRT_HFT_2022231.Repository
+{
+    public class PlayerRepository : Repository<Player>, IRepository<Player>
+    {
+        public PlayerRepository(ApexDbContext context)
+            : base(context) { }
+
+        public override Player Read(int id)
+        {
+            return _context.Players.FirstOrDefault(p => p.PlayerId == id);
+        }
+        public override void Update(Player item)
+        {
+            var old = Read(item.PlayerId);
+            foreach (var prop in old.GetType().GetProperties())
+                prop.SetValue(old, prop.GetValue(item));
+
+            _context.SaveChanges();
+        }
+    }
+}
