97904b08025d7accf622a171c6f347634af0c409
infopek <10saero01@gmail.com>
2022-10-25 21:32:25 +0200

    Create Repository class for Match

---
+++ IVWZRT_HFT_2022231.Repository/ModelRepositories/MatchRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+using IVWZRT_HFT_2022231.Models;
+
+namespace IVWZRT_HFT_2022231.Repository
+{
+    public class MatchRepository : Repository<Match>, IRepository<Match>
+    {
+        public MatchRepository(ApexDbContext context)
+            : base(context) { }
+
+        public override Match Read(int id)
+        {
+            return _context.Matches.FirstOrDefault(p => p.MatchId == id);
+        }
+        public override void Update(Match item)
+        {
+            var old = Read(item.MatchId);
+            foreach (var prop in old.GetType().GetProperties())
+                prop.SetValue(old, prop.GetValue(item));
+
+            _context.SaveChanges();
+        }
+    }
+}
