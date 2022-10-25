a87168e443c74f6e160ce4f028b6d6be7b385651
infopek <10saero01@gmail.com>
2022-10-25 21:32:19 +0200

    Create Repository class for Legend

---
+++ IVWZRT_HFT_2022231.Repository/ModelRepositories/LegendRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+using IVWZRT_HFT_2022231.Models;
+
+namespace IVWZRT_HFT_2022231.Repository
+{
+    public class LegendRepository : Repository<Legend>, IRepository<Legend>
+    {
+        public LegendRepository(ApexDbContext context)
+            : base(context) { }
+
+        public override Legend Read(int id)
+        {
+            return _context.Legends.FirstOrDefault(p => p.LegendId == id);
+        }
+        public override void Update(Legend item)
+        {
+            var old = Read(item.LegendId);
+            foreach (var prop in old.GetType().GetProperties())
+                prop.SetValue(old, prop.GetValue(item));
+
+            _context.SaveChanges();
+        }
+    }
+}
