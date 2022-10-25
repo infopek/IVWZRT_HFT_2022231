63001e103fad03ff03ee0a469e8f296ecc467d75
infopek <10saero01@gmail.com>
2022-10-25 21:31:10 +0200

    Create abstract generic Repository class

---
+++ IVWZRT_HFT_2022231.Repository/GenericRepositories/Repository.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace IVWZRT_HFT_2022231.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        public Repository(ApexDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return _context.Set<T>();
+        }
+        public void Create(T item)
+        {
+            _context.Set<T>().Add(item);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            _context.Set<T>().Remove(Read(id));
+            _context.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+        public abstract void Update(T item);
+        
+        protected ApexDbContext _context;
+    }
+}
