8edd79bda8c2414f1b5be2b23a2ad1e7a846a6a1
infopek <10saero01@gmail.com>
2022-10-26 20:19:19 +0200

    Change include order in EndGameStat

--- IVWZRT_HFT_2022231.Models/EndGameStat.cs
+++ IVWZRT_HFT_2022231.Models/EndGameStat.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IVWZRT_HFT_2022231.Models
 {
