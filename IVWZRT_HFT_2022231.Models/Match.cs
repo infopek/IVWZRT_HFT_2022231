6a363aab2c09ee086479d74c864a5a678f4f29a1
infopek <10saero01@gmail.com>
2022-10-26 20:19:39 +0200

    Change include order in Match

--- IVWZRT_HFT_2022231.Models/Match.cs
+++ IVWZRT_HFT_2022231.Models/Match.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IVWZRT_HFT_2022231.Models
 {
