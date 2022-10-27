d592ae6b2fa6fd21a084de61feef7ca9884c5e8d
infopek <10saero01@gmail.com>
2022-10-27 18:02:36 +0200

    Change include order in ILegendLogic

--- IVWZRT_HFT_2022231.Logic/Interfaces/ILegendLogic.cs
+++ IVWZRT_HFT_2022231.Logic/Interfaces/ILegendLogic.cs
@@ -1,7 +1,7 @@
-﻿using IVWZRT_HFT_2022231.Repository;
-using IVWZRT_HFT_2022231.Models;
 using System.Linq;
 using System.Collections.Generic;
+
+using IVWZRT_HFT_2022231.Models;
 
 namespace IVWZRT_HFT_2022231.Logic
 {
