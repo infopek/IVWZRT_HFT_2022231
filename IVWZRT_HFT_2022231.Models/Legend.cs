12957ff17a372b6260c979ed6088b2379d8cafc1
infopek <10saero01@gmail.com>
2022-10-30 19:40:57 +0100

    Remove all player specific properties from Legend as they ruined the many-to-one relationship between Player and Legend

--- IVWZRT_HFT_2022231.Models/Legend.cs
+++ IVWZRT_HFT_2022231.Models/Legend.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
 
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,42 +7,6 @@ namespace IVWZRT_HFT_2022231.Models
 {
     public class Legend
     {
-        /// <summary>
-        /// Initializes the dictionary containing all 22 legends' info.
-        /// </summary>
-        static Legend()
-        {
-            IDictionary<string, LegendInfo> dict = new Dictionary<string, LegendInfo>()
-            {
-                { "Wraith", new LegendInfo("Into the Void", "Dimensinal Rift", 0) },
-                { "Horizon", new LegendInfo("Gravity Lift", "Black Hole", 0) },
-                { "Fuse", new LegendInfo("Knuckle Cluster", "Dimensinal Rift", 0) },
-                { "Ash", new LegendInfo("Arc Snare", "Phase Breach", 0) },
-                { "Mirage", new LegendInfo("Psyche Out", "Life of the Party", 0) },
-                { "Octane", new LegendInfo("Stim", "Launch Pad", 0) },
-                { "Revenant", new LegendInfo("Silence", "Death Totem", 0) },
-                { "Bangalore", new LegendInfo("Smoke Launcher", "Rolling Thunder", 0) },
-                { "Mad Maggie", new LegendInfo("Riot Drill", "Wrecking Ball", 0) },
-
-                { "Rampart", new LegendInfo("Amped Cover", "Mobile Minigun \"Sheila\"", 1) },
-                { "Gibraltar", new LegendInfo("Dome of Protection", "Defensive Bombardment", 1) },
-                { "Newcastle", new LegendInfo("Mobile Shield", "Castle Wall", 1) },
-                { "Caustic", new LegendInfo("Nox Gas Trap", "Nox Gas Grenade", 1) },
-                { "Wattson", new LegendInfo("Perimeter Security", "Interception Pylon", 1) },
-
-                { "Loba", new LegendInfo("Burglar's Best Friend", "Black Market Boutique", 2) },
-                { "Lifeline", new LegendInfo("D.O.C Heal Drone", "Care Package", 2) },
-
-                { "Vantage", new LegendInfo("Echo Relocation", "Sniper's Mark", 3) },
-                { "Bloodhound", new LegendInfo("Eye of the Allfather", "Beast of the Hunt", 3) },
-                { "Seer", new LegendInfo("Focus of Attention", "Exhibit", 3) },
-                { "Pathfinder", new LegendInfo("Grappling Hook", "Zipline Gun", 3) },
-                { "Valkyrie", new LegendInfo("Missile Swarm", "Skyward Dive", 3) },
-                { "Crypto", new LegendInfo("Surveillance Drone", "Drone Emp", 3) },
-            };
-            _legends = new ReadOnlyDictionary<string, LegendInfo>(dict);
-        }
-
         public Legend() { }
         public Legend(string line)
         {
@@ -51,25 +14,18 @@ namespace IVWZRT_HFT_2022231.Models
 
             LegendId = int.Parse(split[0]);
 
-            Info = _legends[split[1]];
-
+            Name = split[1];
             Skin = split[2];
-            Revives = int.Parse(split[3]);
-            Headshots = int.Parse(split[4]);
         }
 
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int LegendId { get; set; }
 
-        public LegendInfo Info { get; private set; }
-
+        [Required]
+        [StringLength(20)]
         public string Skin { get; set; }
-        public int Revives { get; set; }
-        public int Headshots { get; set; }
 
         public virtual ICollection<Player> Players { get; set; }
-
-        private static readonly IReadOnlyDictionary<string, LegendInfo> _legends;
     }
 }
