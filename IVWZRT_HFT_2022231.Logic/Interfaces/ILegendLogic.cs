9a61ec83519f2dc6b9253fef3d02d9f464213740
infopek <10saero01@gmail.com>
2022-10-30 19:45:47 +0100

    Remove non-crud queries in ILegendLogic

--- IVWZRT_HFT_2022231.Logic/Interfaces/ILegendLogic.cs
+++ IVWZRT_HFT_2022231.Logic/Interfaces/ILegendLogic.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using System.Collections.Generic;
 
 using IVWZRT_HFT_2022231.Models;
 
@@ -13,9 +12,5 @@ namespace IVWZRT_HFT_2022231.Logic
         void Create(Legend item);
         void Update(Legend item);
         void Delete(int id);
-
-        // NON-CRUD
-        IEnumerable<Legend> MostUsedLegendByRank();
-        IEnumerable<int> NumGamesByDefensiveLegend();
     }
 }
