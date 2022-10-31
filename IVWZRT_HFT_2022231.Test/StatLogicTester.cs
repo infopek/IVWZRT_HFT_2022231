bea153dd72e1c314dbb38cd082557bdf7ca07150
infopek <10saero01@gmail.com>
2022-10-31 11:26:42 +0100

    Create Init method that sets up _logic with Moq

--- IVWZRT_HFT_2022231.Test/StatLogicTester.cs
+++ IVWZRT_HFT_2022231.Test/StatLogicTester.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using NUnit.Framework;
 using Moq;
 
 using IVWZRT_HFT_2022231.Logic;
@@ -7,8 +10,24 @@ using IVWZRT_HFT_2022231.Models;
 
 namespace IVWZRT_HFT_2022231.Test
 {
+    [TestFixture]
     public class StatLogicTester
     {
+        [SetUp]
+        public void Init()
+        {
+            _mockLegendRepo = new Mock<IRepository<EndGameStat>>();
+            _mockLegendRepo.Setup(lr => lr.ReadAll()).Returns(new List<EndGameStat>()
+            {
+                new EndGameStat("14@10@5@1000@5@7"),
+                new EndGameStat("15@11@6@1100@6@6"),
+                new EndGameStat("16@12@7@1200@7@5")
+            }.AsQueryable());
 
+            _logic = new StatLogic(_mockLegendRepo.Object);
+        }
+
+        private StatLogic _logic;
+        private Mock<IRepository<EndGameStat>> _mockLegendRepo;
     }
 }
