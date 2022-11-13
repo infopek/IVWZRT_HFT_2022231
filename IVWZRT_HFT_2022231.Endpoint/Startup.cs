c1abb1ea8fdb874c65ebf8f40d523905b56ebcc9
infopek <10saero01@gmail.com>
2022-11-13 18:04:18 +0100

    Implement Startup so that it initialized Controllers for Endpoint

--- IVWZRT_HFT_2022231.Endpoint/Startup.cs
+++ IVWZRT_HFT_2022231.Endpoint/Startup.cs
@@ -1,40 +1,73 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Diagnostics;
+
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+using Microsoft.OpenApi.Models;
+
+using IVWZRT_HFT_2022231.Logic;
+using IVWZRT_HFT_2022231.Repository;
+using IVWZRT_HFT_2022231.Models;
 
 namespace IVWZRT_HFT_2022231.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ApexDbContext>();
+
+            services.AddTransient<IRepository<Player>, PlayerRepository>();
+            services.AddTransient<IRepository<Legend>, LegendRepository>();
+            services.AddTransient<IRepository<Match>, MatchRepository>();
+            services.AddTransient<IRepository<EndGameStat>, StatRepository>();
+
+            services.AddTransient<IPlayerLogic, PlayerLogic>();
+            services.AddTransient<ILegendLogic, LegendLogic>();
+            services.AddTransient<IMatchLogic, MatchLogic>();
+            services.AddTransient<IStatLogic, StatLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "IVWZRT_HFT_2022231.Endpoint", Version = "v1" });
+            });
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IVWZRT_HFT_2022231.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
+
+        public IConfiguration Configuration { get; }
     }
 }
