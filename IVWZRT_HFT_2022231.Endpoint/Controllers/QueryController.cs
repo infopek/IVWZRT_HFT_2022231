af905882002c8c57f6046b5e973f48fa220be98b
infopek <10saero01@gmail.com>
2022-11-13 18:02:50 +0100

    Add QueryController to Endpoint that enables the use of non-crud methods in swagger api

---
+++ IVWZRT_HFT_2022231.Endpoint/Controllers/QueryController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+using IVWZRT_HFT_2022231.Logic;
+using IVWZRT_HFT_2022231.Models;
+
+namespace IVWZRT_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class QueryController : ControllerBase
+    {
+        public QueryController(IPlayerLogic playerLogic, IMatchLogic matchLogic)
+        {
+            _playerLogic = playerLogic;
+            _matchLogic = matchLogic;
+        }
+
+        // Player queries
+        [HttpGet("{rank}")]
+        public IEnumerable<PlayerLogic.PlayerRankInfo> PlayersWithGreaterKD(string rank)
+        {
+            return _playerLogic.PlayersWithGreaterKD(rank);
+        }
+        [HttpGet("{username}")]
+        public int NumTimesTopThree(string username)
+        {
+            return _playerLogic.NumTimesTopThree(username);
+        }
+
+        // Match queries
+        [HttpGet("{gameMode}")]
+        public float AvgLengthOfGame(string gameMode)
+        {
+            return _matchLogic.AvgLengthOfGame(gameMode);
+        }
+        [HttpGet]
+        public IEnumerable<string> MapsWithMostRamparts()
+        {
+            return _matchLogic.MapsWithMostRamparts();
+        }
+        [HttpGet]
+        public IEnumerable<Match> LongestMatchesInDiamond()
+        {
+            return _matchLogic.LongestMatchesInDiamond();
+        }
+
+        private readonly IPlayerLogic _playerLogic;
+        private readonly IMatchLogic _matchLogic;
+    }
+}
