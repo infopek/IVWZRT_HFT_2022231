66b80a0f1debdd23c28e98201645a9a205e0479f
infopek <10saero01@gmail.com>
2022-11-13 18:01:58 +0100

    Add StatController to Endpoint

---
+++ IVWZRT_HFT_2022231.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+using IVWZRT_HFT_2022231.Logic;
+using IVWZRT_HFT_2022231.Models;
+
+namespace IVWZRT_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        public StatController(IStatLogic logic)
+        {
+            _logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<EndGameStat> ReadAll()
+        {
+            return _logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public EndGameStat Read(int id)
+        {
+            return _logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] EndGameStat stat)
+        {
+            _logic.Create(stat);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] EndGameStat stat)
+        {
+            _logic.Update(stat);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logic.Delete(id);
+        }
+
+        private readonly IStatLogic _logic;
+    }
+}
